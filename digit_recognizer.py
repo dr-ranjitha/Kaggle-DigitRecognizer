8bcb1faa73fbbf454814e5189f39cd3b7c7dbfaf
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-30 11:30:18 -0600

    Create digit_recognizer.py
    importing libraries

---
+++ digit_recognizer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 11:20:39 2020
+
+@author: drran
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+%matplotlib inline
