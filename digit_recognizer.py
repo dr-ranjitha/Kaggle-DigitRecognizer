4dee5a39295c0a18b5d7f2b0d397658a52069cb4
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-30 11:30:39 -0600

    Update digit_recognizer.py
    Importing dataset

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -4,10 +4,14 @@ Created on Thu Apr 30 11:20:39 2020
 
 @author: drran
 """
-
+# Importing the libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import seaborn as sns
 %matplotlib inline
+
+# Importing the dataset
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
